9c57b0732a008ce55d593c2bb1bfb35fb259da4f
kimkihyeon <rlgus4531@naver.com>
2025-10-12 23:49:14 +0900

    검증 직접 처리 - 개발

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -4,14 +4,19 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -38,7 +43,8 @@ public class BasicItemController {
     }
 
     @GetMapping("/add")
-    public String addView() {
+    public String addView(Model model) {
+        model.addAttribute("item", new Item());
         return "basic/addForm";
     }
 
@@ -78,7 +84,36 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String saveV4(Item item, RedirectAttributes redirectAttributes) { // @ModelAttribute생략 가능
+    public String saveV4(Item item, RedirectAttributes redirectAttributes, Model model) { // @ModelAttribute생략 가능
+
+        //검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1000원에서 1000000까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "수량은 최대 9999개까지 허용합니다.");
+        }
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 x 수량의 합은 10000원 이상이어야 합니다." + resultPrice);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()) { // 에러에 값이 담겨있으면 검증에 걸린 것
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+            return "basic/addForm"; // 상품 추가 입력 폼
+        }
+
+        //상품 입력 성공
         Item saved = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", saved.getId());
         redirectAttributes.addAttribute("status", true);
